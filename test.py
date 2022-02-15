7b1bdf291b94f9dbaba203aa5bd2be3c2af94253
hyunsik4476 <hyunsik4476@gmail.com>
2022-02-15 22:28:12 +0900

    약간 수정

---
+++ test.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+a = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
+pprint(a)
+b = list(map(list, zip(*a)))
+pprint(b)
+# c = []
+# for i in range(len(b)):
+#     c.append(b[i][::-1])
+# pprint(c)
+d = a[::-1]
+pprint(d)
