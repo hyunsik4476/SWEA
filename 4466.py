a438cad40d06f49f0d69eed8d82714d10b3ff349
hyunsik4476 <hyunsik4476@gmail.com>
2022-03-01 20:18:10 +0900

    SWEA

---
+++ 4466.py
@@ -0,0 +1,7 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+    lst = list(map(int, input().split()))
+    lst.sort(reverse=True)
+    print(f'#{tc} {sum(lst[:K])}')
